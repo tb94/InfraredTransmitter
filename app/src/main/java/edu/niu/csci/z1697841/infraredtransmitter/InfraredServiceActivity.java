6b803a771a2183e89b4601ebb39fc66de4515c18
Tim <tbretz94@gmail.com>
2018-03-09 20:06:46 -0600

    Updated to ConsumerIrManager for api 19+

--- app/src/main/java/edu/niu/csci/z1697841/infraredtransmitter/InfraredServiceActivity.java
+++ app/src/main/java/edu/niu/csci/z1697841/infraredtransmitter/InfraredServiceActivity.java
@@ -3,9 +3,13 @@ package edu.niu.csci.z1697841.infraredtransmitter;
 import android.app.Activity;
 import android.content.Context;
 import android.hardware.ConsumerIrManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.SparseArray;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.lang.reflect.Array;
 import java.lang.reflect.InvocationTargetException;
@@ -14,24 +18,53 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/*
+* Class: InfraredServiceActivity
+* Inherits from: Activity
+* Sends IR pattern to receiver
+* */
 public class InfraredServiceActivity extends Activity {
-    Object irdaService;
-    Method irWrite;
-    ConsumerIrManager consumerIrManager;
-    SparseArray<String> irData;
+    private Method irWrite;
+    private ConsumerIrManager irManager;
+    private SparseArray<String> irData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_infrared_service);
 
-        irData = new SparseArray<>();
+        irManager = (ConsumerIrManager) getSystemService(CONSUMER_IR_SERVICE);
+        final TextView displayTV = findViewById(R.id.displayTextView);
+        Button sendBtn = findViewById(R.id.sendButton);
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StringBuilder values = new StringBuilder("IR Carrier Frequencies:\n\n");
+                if (irManager.hasIrEmitter()) {
+                    ConsumerIrManager.CarrierFrequencyRange[] frequencyRanges = irManager.getCarrierFrequencies();
+
+                    for (ConsumerIrManager.CarrierFrequencyRange range : frequencyRanges) {
+                        values.append(range.getMinFrequency());
+                        values.append(" - ");
+                        values.append(range.getMaxFrequency());
+                        values.append("\n");
+                    }
+                    displayTV.setText(values.toString());
+                } else {
+                    displayTV.setText("Device does not have IR Emitter");
+                }
+            }
+        });
+
+/*        irData = new SparseArray<>();
 
         irData.put(R.id.sendButton, hexToDec("0000 006d 0022 0002 0152 00aa 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 003f 0015 0015 0015 0015 0015 0015 0015 0015 0015 003f 0015"
                 + " 003f 0015 003f 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 003f 0015 003f 0015 003f 0015 003f 0015 003f 0015 003f 0015 003f 0015 0015 0015 0696 0152 0055 0015 0e23"));
-        irInit();
+        irInit();*/
     }
 
+    /*
     public void irSend(View view) {
         String data = irData.get(view.getId());
         if (data != null) {
@@ -69,5 +102,5 @@ public class InfraredServiceActivity extends Activity {
 
     private void irInit() {
         consumerIrManager = (ConsumerIrManager)getSystemService(Context.CONSUMER_IR_SERVICE);
-    }
+    }*/
 }
