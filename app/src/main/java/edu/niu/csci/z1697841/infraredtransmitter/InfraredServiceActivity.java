c1240d8a9041da9711abb545688dae5e75794f3a
Tim <tbretz94@gmail.com>
2018-03-11 15:43:28 -0500

    Updated to ConsumerIrManager for api 19+

--- app/src/main/java/edu/niu/csci/z1697841/infraredtransmitter/InfraredServiceActivity.java
+++ app/src/main/java/edu/niu/csci/z1697841/infraredtransmitter/InfraredServiceActivity.java
@@ -27,6 +27,11 @@ public class InfraredServiceActivity extends Activity {
     private Method irWrite;
     private ConsumerIrManager irManager;
     private SparseArray<String> irData;
+    private int[] pattern = {1901, 4453, 625, 1614, 625, 1588, 625, 1614, 625, 442, 625, 442, 625,
+            468, 625, 442, 625, 494, 572, 1614, 625, 1588, 625, 1614, 625, 494, 572, 442, 651,
+            442, 625, 442, 625, 442, 625, 1614, 625, 1588, 651, 1588, 625, 442, 625, 494, 598,
+            442, 625, 442, 625, 520, 572, 442, 625, 442, 625, 442, 651, 1588, 625, 1614, 625,
+            1588, 625, 1614, 625, 1588, 625, 48958};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,67 +45,15 @@ public class InfraredServiceActivity extends Activity {
         sendBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                StringBuilder values = new StringBuilder("IR Carrier Frequencies:\n\n");
+                StringBuilder values = new StringBuilder("Sending signal at Frequency:\n\n");
                 if (irManager.hasIrEmitter()) {
-                    ConsumerIrManager.CarrierFrequencyRange[] frequencyRanges = irManager.getCarrierFrequencies();
-
-                    for (ConsumerIrManager.CarrierFrequencyRange range : frequencyRanges) {
-                        values.append(range.getMinFrequency());
-                        values.append(" - ");
-                        values.append(range.getMaxFrequency());
-                        values.append("\n");
-                    }
+                    values.append("38000");
+                    irManager.transmit(38000, pattern);
                     displayTV.setText(values.toString());
                 } else {
                     displayTV.setText("Device does not have IR Emitter");
                 }
             }
         });
-
-/*        irData = new SparseArray<>();
-
-        irData.put(R.id.sendButton, hexToDec("0000 006d 0022 0002 0152 00aa 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 003f 0015 0015 0015 0015 0015 0015 0015 0015 0015 003f 0015"
-                + " 003f 0015 003f 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 003f 0015 003f 0015 003f 0015 003f 0015 003f 0015 003f 0015 003f 0015 0015 0015 0696 0152 0055 0015 0e23"));
-        irInit();*/
     }
-
-    /*
-    public void irSend(View view) {
-        String data = irData.get(view.getId());
-        if (data != null) {
-            try {
-                irWrite.invoke(consumerIrManager, data);
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            } catch (InvocationTargetException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private String hexToDec(String irData) {
-        List<String> list = new ArrayList<>(Arrays.asList(irData.split(" ")));
-        list.remove(0); // dummy
-        int frequency = Integer.parseInt(list.remove(0), 16);
-        list.remove(0); // seq1
-        list.remove(0); // seq2
-
-        for (int i = 0; i < list.size(); i++) {
-            list.set(i, Integer.toString(Integer.parseInt(list.get(i), 16)));
-        }
-
-        frequency = (int) (1000000 / (frequency * 0.241246));
-        list.add(Integer.toString(frequency));
-
-        irData = " ";
-        for (String s: list) {
-            irData += s + ".";
-        }
-
-        return irData;
-    }
-
-    private void irInit() {
-        consumerIrManager = (ConsumerIrManager)getSystemService(Context.CONSUMER_IR_SERVICE);
-    }*/
 }
