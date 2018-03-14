8daed067abd34145256a7f7ba43886ca376c7f10
Tim <tbretz94@gmail.com>
2018-03-13 22:11:24 -0500

    Updated to ConsumerIrManager for api 19+

--- app/src/main/java/edu/niu/csci/z1697841/infraredtransmitter/InfraredServiceActivity.java
+++ app/src/main/java/edu/niu/csci/z1697841/infraredtransmitter/InfraredServiceActivity.java
@@ -1,40 +1,79 @@
 package edu.niu.csci.z1697841.infraredtransmitter;
 
 import android.app.Activity;
-import android.content.Context;
 import android.hardware.ConsumerIrManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.lang.reflect.Array;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 /*
 * Class: InfraredServiceActivity
 * Inherits from: Activity
-* Sends IR pattern to receiver
+* Sends IR patternA to receiver
 * */
 public class InfraredServiceActivity extends Activity {
+
     private Method irWrite;
     private ConsumerIrManager irManager;
     private SparseArray<String> irData;
-    private int[] pattern = {1901, 4453, 625, 1614, 625, 1588, 625, 1614, 625, 442, 625, 442, 625,
-            468, 625, 442, 625, 494, 572, 1614, 625, 1588, 625, 1614, 625, 494, 572, 442, 651,
-            442, 625, 442, 625, 442, 625, 1614, 625, 1588, 651, 1588, 625, 442, 625, 494, 598,
-            442, 625, 442, 625, 520, 572, 442, 625, 442, 625, 442, 651, 1588, 625, 1614, 625,
-            1588, 625, 1614, 625, 1588, 625, 48958};
+    private int[] patternA =
+    // 38400 Hz
+    {
+
+            // 9ms leading pulse burst (16 times the pulse burst length used for a logical data bit)
+            //    http://techdocs.altium.com/display/FPGA/NEC+Infrared+Transmission+Protocol
+
+            9000, 4500,
+
+
+            // lead in burst
+            // http://www.hifi-remote.com/infrared/IR-PWM.shtml
+            // 0, 108, 34, 0, 346, 173,
+
+            // 8 bit address for the receiving device address
+            // 1000 0001
+            22, 65, 22, 22,
+            22, 22, 22, 22,
+            22, 22, 22, 22,
+            22, 22, 22, 65,
+
+            // 8 bit logical inverse for the receiving device address
+            // 0111 1110
+            22, 22, 22, 65,
+            22, 65, 22, 65,
+            22, 65, 22, 65,
+            22, 65, 22, 22,
+
+            // 8 bit command
+            // 0001 0001
+            22, 22, 22, 22,
+            22, 22, 22, 65,
+            22, 22, 22, 22,
+            22, 22, 22, 65,
+
+            // 8 bit logical inverse of command
+            // 1110 1110
+            22, 65, 22, 65,
+            22, 65, 22, 22,
+            22, 65, 22, 65,
+            22, 65, 22, 22,
+
+            // end of message transmission
+            22
+
+            // lead out burst
+            // 22, 1427
+
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_infrared_service);
 
@@ -45,10 +84,11 @@ public class InfraredServiceActivity extends Activity {
         sendBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 StringBuilder values = new StringBuilder("Sending signal at Frequency:\n\n");
                 if (irManager.hasIrEmitter()) {
-                    values.append("38000");
-                    irManager.transmit(38000, pattern);
+                    values.append("38400");
+                    irManager.transmit(38400, patternA);
                     displayTV.setText(values.toString());
                 } else {
                     displayTV.setText("Device does not have IR Emitter");
