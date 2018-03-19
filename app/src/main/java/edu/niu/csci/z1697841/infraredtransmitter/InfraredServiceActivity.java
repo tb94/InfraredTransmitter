839163461ffe5eb4bd9fe89fbfd0a37e572cd597
Tim <tbretz94@gmail.com>
2018-03-19 15:28:14 -0500

    Added pattern received from IR remote that came with IR receiver. Would have done this sooner if I had a CR2025

--- app/src/main/java/edu/niu/csci/z1697841/infraredtransmitter/InfraredServiceActivity.java
+++ app/src/main/java/edu/niu/csci/z1697841/infraredtransmitter/InfraredServiceActivity.java
@@ -20,9 +20,26 @@ public class InfraredServiceActivity extends Activity {
     private Method irWrite;
     private ConsumerIrManager irManager;
     private SparseArray<String> irData;
-    private int[] patternA =
-    // 38400 Hz
-    {
+    private int [] patternB = {
+            // 38400 Hz
+
+            // collected from remote that came with IR receiver
+
+            // head
+            8986, 4614,
+
+            494, 646, 494, 650, 486, 642,
+            522, 618, 522, 614, 490, 646, 498, 642,
+            498, 638, 470, 1782, 490, 1754, 502, 1750,
+            498, 1754, 522, 1730, 490, 1758, 522, 1726,
+            498, 1754, 494, 646, 490, 1754, 526, 1730,
+            486, 646, 494, 1754, 522, 618, 494, 646,
+            490, 642, 502, 1750, 522, 622, 486, 646,
+            522, 1722, 498, 642, 502, 1750, 498, 1750,
+            498, 1758, 490, 1000
+    };
+    private int[] patternA = {
+            // 38400 Hz
 
             // 9ms leading pulse burst (16 times the pulse burst length used for a logical data bit)
             //    http://techdocs.altium.com/display/FPGA/NEC+Infrared+Transmission+Protocol
@@ -88,7 +105,7 @@ public class InfraredServiceActivity extends Activity {
                 StringBuilder values = new StringBuilder("Sending signal at Frequency:\n\n");
                 if (irManager.hasIrEmitter()) {
                     values.append("38400");
-                    irManager.transmit(38400, patternA);
+                    irManager.transmit(38400, patternB);
                     displayTV.setText(values.toString());
                 } else {
                     displayTV.setText("Device does not have IR Emitter");
