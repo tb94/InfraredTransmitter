0a1e47e3b3d13bd63290c9908693a33e2c2f5674
Tim <tbretz94@gmail.com>
2018-04-28 12:55:03 -0500

    Added Info page and button. Adjusted IR pattern to match NEC protocol more accurately.

---
+++ app/src/main/java/edu/niu/csci/z1697841/infraredtransmitter/InfoActivity.java
@@ -0,0 +1,13 @@
+package edu.niu.csci.z1697841.infraredtransmitter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InfoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
--- app/src/main/java/edu/niu/csci/z1697841/infraredtransmitter/InfraredServiceActivity.java
+++ app/src/main/java/edu/niu/csci/z1697841/infraredtransmitter/InfraredServiceActivity.java
@@ -1,6 +1,7 @@
 package edu.niu.csci.z1697841.infraredtransmitter;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.hardware.ConsumerIrManager;
 import android.os.Bundle;
 import android.view.View;
@@ -24,89 +25,65 @@ public class InfraredServiceActivity extends Activity implements Animation.Anima
     Button rtlBtn;
     Button ltrBtn;
     ImageView couch;
+    Button infoBtn;
 
     Animation rtlAnimation;
     Animation ltrAnimation;
     private ConsumerIrManager irManager;
     private boolean ltr = false;
     private boolean rtl = false;
-    private int[] patternLEFT = {
+    private int[] patternLTR = {
             // 38400 Hz
-            // collected from remote that came with IR receiver
-            8986, 4614,
-            494, 646, 494, 650, 486, 642,
-            522, 618, 522, 614, 490, 646, 498, 642,
-            498, 638, 470, 1782, 490, 1754, 502, 1750,
-            498, 1754, 522, 1730, 490, 1758, 522, 1726,
-            498, 1754, 494, 646, 490, 1754, 526, 1730,
-            486, 646, 494, 1754, 522, 618, 494, 646,
-            490, 642, 502, 1750, 522, 622, 486, 646,
-            522, 1722, 498, 642, 502, 1750, 498, 1750,
-            498, 1758, 490, 1000
-    };
-    private int[] patternRIGHT = {
-            // 38400 Hz
-            // collected from remote that came with IR receiver
-            8986, 4614,
-            494, 646, 494, 650, 486, 642,
-            522, 618, 522, 614, 490, 646, 498, 642,
-            498, 638, 470, 1782, 490, 1754, 502, 1750,
-            498, 1754, 522, 1730, 490, 1758, 522, 1726,
-            498, 1754, 494, 646, 490, 1754, 526, 1730,
-            486, 646, 494, 1754, 522, 618, 494, 646,
-            490, 642, 502, 1750, 522, 622, 486, 646,
-            522, 1722, 498, 642, 502, 1750, 498, 1750,
-            498, 1758, 490, 1000
-    };
-    /*
-    * DOES NOT CURRENTLY WORK
-    */
-    private int[] patternA = {
-            // 38400 Hz
-            // 9ms leading pulse burst (16 times the pulse burst length used for a logical data bit)
-            //    http://techdocs.altium.com/display/FPGA/NEC+Infrared+Transmission+Protocol
+            // NEC protocol 0xFF02FD
 
+            // Head:
             9000, 4500,
 
-            // lead in burst
-            // http://www.hifi-remote.com/infrared/IR-PWM.shtml
-            // 0, 108, 34, 0, 346, 173,
+            // Address:
+            563, 563, 563, 563, 563, 563, 563, 563,
+            563, 563, 563, 563, 563, 563, 563, 563,
 
-            // 8 bit address for the receiving device address
-            // 1000 0001
-            22, 65, 22, 22,
-            22, 22, 22, 22,
-            22, 22, 22, 22,
-            22, 22, 22, 65,
+            // Address Inverse:
+            563, 1688, 563, 1688, 563, 1688, 563, 1688,
+            563, 1688, 563, 1688, 563, 1688, 563, 1688,
 
-            // 8 bit logical inverse for the receiving device address
-            // 0111 1110
-            22, 22, 22, 65,
-            22, 65, 22, 65,
-            22, 65, 22, 65,
-            22, 65, 22, 22,
+            // Command:
+            563, 563, 563, 563, 563, 563, 563, 563,
+            563, 563, 563, 563, 563, 1688, 563, 563,
 
-            // 8 bit command
-            // 0001 0001
-            22, 22, 22, 22,
-            22, 22, 22, 65,
-            22, 22, 22, 22,
-            22, 22, 22, 65,
+            // Command Inverse:
+            563, 1688, 563, 1688, 563, 1688, 563, 1688,
+            563, 1688, 563, 1688, 563, 563, 563, 1688,
 
-            // 8 bit logical inverse of command
-            // 1110 1110
-            22, 65, 22, 65,
-            22, 65, 22, 22,
-            22, 65, 22, 65,
-            22, 65, 22, 22,
-
-            // end of message transmission
-            22
-
-            // lead out burst
-            // 22, 1427
+            // End Transmission
+            563
     };
+    private int[] patternRTL = {
+            // 38400 Hz
+            // NEC protocol 0xFF22DD
 
+            // Head:
+            9000, 4500,
+
+            // Address:
+            563, 563, 563, 563, 563, 563, 563, 563,
+            563, 563, 563, 563, 563, 563, 563, 563,
+
+            // Address Inverse:
+            563, 1688, 563, 1688, 563, 1688, 563, 1688,
+            563, 1688, 563, 1688, 563, 1688, 563, 1688,
+
+            // Command:
+            563, 563, 563, 563, 563, 1688, 563, 563,
+            563, 563, 563, 563, 563, 1688, 563, 563,
+
+            // Command Inverse:
+            563, 1688, 563, 1688, 563, 563, 563, 1688,
+            563, 1688, 563, 1688, 563, 563, 563, 1688,
+
+            // End Transmission
+            563
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -119,10 +96,19 @@ public class InfraredServiceActivity extends Activity implements Animation.Anima
         ltrBtn = findViewById(R.id.ltrButton);
         rtlBtn = findViewById(R.id.rtlButton);
         couch = findViewById(R.id.couchView);
+        infoBtn = findViewById(R.id.infoButton);
         rtlBtn.setBackgroundColor(BLUE);
         ltrBtn.setBackgroundColor(BLUE);
         ltrAnimation = AnimationUtils.loadAnimation(InfraredServiceActivity.this, R.anim.ltr_anim);
         rtlAnimation = AnimationUtils.loadAnimation(InfraredServiceActivity.this, R.anim.rtl_anim);
+
+        infoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(InfraredServiceActivity.this, InfoActivity.class);
+                startActivity(intent);
+            }
+        });
 
         rtlBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -151,14 +137,14 @@ public class InfraredServiceActivity extends Activity implements Animation.Anima
                 if (!irManager.hasIrEmitter()) {
                     Toast.makeText(InfraredServiceActivity.this, "Device does not have IR capabilities", Toast.LENGTH_LONG).show();
                     return;
-                } else {
-                    irManager.transmit(38400, patternLEFT);
                 }
 
                 if (rtl) {
+                    irManager.transmit(38400, patternRTL);
                     ltrBtn.setVisibility(View.INVISIBLE);
                     rtlBtn.startAnimation(rtlAnimation);
                 } else if (ltr) {
+                    irManager.transmit(38400, patternLTR);
                     rtlBtn.setVisibility(View.INVISIBLE);
                     ltrBtn.startAnimation(ltrAnimation);
                 }
@@ -188,6 +174,5 @@ public class InfraredServiceActivity extends Activity implements Animation.Anima
 
     @Override
     public void onAnimationRepeat(Animation animation) {
-
     }
 }
